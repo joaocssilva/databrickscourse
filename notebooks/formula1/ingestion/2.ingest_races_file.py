f072bfb67375f8548be9a469f7acb69eae9ef242
joaocssilva <78818992+joaocssilva@users.noreply.github.com>
2021-11-01 21:53:16 +0000

    Done

---
+++ notebooks/formula1/ingestion/2.ingest_races_file.py
@@ -0,0 +1,85 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest races.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 1 - Read de CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+
+# COMMAND ----------
+
+races_schema = StructType(fields= [StructField("raceId", IntegerType(), False),
+                                      StructField("year", IntegerType(), True),
+                                      StructField("round", IntegerType(), True),
+                                      StructField("circuitId", IntegerType(), True),
+                                      StructField("name", StringType(), True),
+                                      StructField("date", DateType(), True),
+                                      StructField("time", StringType(), True),
+                                      StructField("url", StringType(), True)
+                                     ])
+
+# COMMAND ----------
+
+races_df = spark.read \
+.option("header", True) \
+.schema(races_schema) \
+.csv("/mnt/formula1joaodl/raw/races.csv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step2 - Select only the required columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+races_selected_df = races_df.select(col("raceId"),col("year"),col("round"),col("circuitId"),col("name"),col("date"),col("time"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3 - Rename the columns as required
+
+# COMMAND ----------
+
+races_renamed_df = races_selected_df.withColumnRenamed("raceId", "race_id")\
+.withColumnRenamed("year", "race_year") \
+.withColumnRenamed("circuitId", "circuit_id")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 4 - Add ingestion date and race timestamp to the dataframe
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, lit, to_timestamp, concat
+
+# COMMAND ----------
+
+races_final_df = races_renamed_df.withColumn("race_timestamp", to_timestamp(concat(col("date"), lit(" "), col("time")), "yyyy-MM-dd HH:mm:ss")) \
+.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 5 - Write data to datalake as parquet
+
+# COMMAND ----------
+
+races_final_df.write.mode("overwrite").partitionBy('race_year').parquet("/mnt/formula1joaodl/processed/races")
+
+# COMMAND ----------
+
+display(spark.read.parquet('/mnt/formula1joaodl/processed/races'))
+
+# COMMAND ----------
+
